dc98e74fc50d44bc13cbd0debbc604ba83e441d1
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-02-08 23:39:52 +0530

    remove duplicate example

--- with-gatsby2/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const Layout = require('./src/components/layout').default;
-
-// Wraps every page in a component
-exports.wrapPageElement = ({ element, props }) => {
-	return <Layout {...props}>{element}</Layout>;
-};
