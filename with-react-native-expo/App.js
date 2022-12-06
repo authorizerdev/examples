abdb3d7d88438f4db916b308ad0b9a3adfc78b8e
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-12-06 11:58:02 +0530

    update with-react-native-expo example

--- with-react-native-expo/App.js
+++ with-react-native-expo/App.js
@@ -1,169 +1,173 @@
-import * as AuthSession from 'expo-auth-session';
-import jwtDecode from 'jwt-decode';
-import * as SecureStore from 'expo-secure-store';
-import { useEffect, useState } from 'react';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
-import { Authorizer } from '@authorizerdev/authorizer-js';
+import * as AuthSession from "expo-auth-session";
+import jwtDecode from "jwt-decode";
+import * as SecureStore from "expo-secure-store";
+import { useEffect, useState } from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { Authorizer } from "@authorizerdev/authorizer-js";
 
 // You need to swap out the Authorizer client id and domain with the one from your Authorizer client.
 // In your Authorizer client, you need to also add a url to your authorized redirect urls.
 
 const useProxy = false;
 const redirectUri = AuthSession.makeRedirectUri({ useProxy });
+console.log(redirectUri);
 
-const authorizerClientID = '96fed66c-9779-4694-a79a-260fc489ce33';
-const authorizerURL = 'https://demo.authorizer.dev';
+const authorizerClientID = "96fed66c-9779-4694-a79a-260fc489ce33";
+const authorizerURL = "https://demo.authorizer.dev";
 const authorizationEndpoint = `${authorizerURL}/authorize`;
 const authorizerRef = new Authorizer({
-	clientID: authorizerClientID,
-	authorizerURL: authorizerURL,
-	redirectURL: redirectUri,
+  clientID: authorizerClientID,
+  authorizerURL: authorizerURL,
+  redirectURL: redirectUri,
 });
 
 const authorizerRefreshTokenKey = `authorizer_refresh_token`;
 
 export default function App() {
-	const [email, setEmail] = useState(null);
-	const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-	const [request, result, promptAsync] = AuthSession.useAuthRequest(
-		{
-			redirectUri,
-			clientId: authorizerClientID,
-			// id_token will return a JWT token
-			responseType: 'token',
-			// use offline access to get a refresh token and perform silent refresh in background
-			scopes: ['openid', 'profile', 'email', 'offline_access'],
-			extraParams: {
-				// ideally, this will be a random value
-				nonce: 'nonce',
-			},
-		},
-		{ authorizationEndpoint }
-	);
+  const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    {
+      redirectUri,
+      clientId: authorizerClientID,
+      // id_token will return a JWT token
+      responseType: "token",
+      // use offline access to get a refresh token and perform silent refresh in background
+      scopes: ["openid", "profile", "email", "offline_access"],
+      extraParams: {
+        // ideally, this will be a random value
+        nonce: "nonce",
+      },
+    },
+    { authorizationEndpoint }
+  );
 
-	// on init silently refresh token if it exists
-	useEffect(() => {
-		async function silentRefresh() {
-			try {
-				const refreshToken = await SecureStore.getItemAsync(
-					authorizerRefreshTokenKey
-				);
-				if (refreshToken) {
-					try {
-						const res = await authorizerRef.getToken({
-							grant_type: 'refresh_token',
-							refresh_token: refreshToken,
-						});
-						await SecureStore.setItemAsync(
-							'authorizer_refresh_token',
-							res.refresh_token
-						);
-						setEmail(jwtDecode(res.id_token).email);
-					} catch (err) {
-						console.error(err);
-						await SecureStore.deleteItemAsync(authorizerRefreshTokenKey);
-					}
-				}
-			} catch (error) {
-				setEmail(null);
-				await SecureStore.deleteItemAsync(authorizerRefreshTokenKey);
-			} finally {
-				setLoading(false);
-			}
-		}
-		silentRefresh();
-	}, []);
+  // on init silently refresh token if it exists
+  useEffect(() => {
+    async function silentRefresh() {
+      try {
+        const refreshToken = await SecureStore.getItemAsync(
+          authorizerRefreshTokenKey
+        );
+        console.log(refreshToken);
+        if (refreshToken) {
+          try {
+            const res = await authorizerRef.getToken({
+              grant_type: "refresh_token",
+              refresh_token: refreshToken,
+            });
+            console.log({ res });
+            await SecureStore.setItemAsync(
+              "authorizer_refresh_token",
+              res.refresh_token
+            );
 
-	useEffect(() => {
-		async function setResult() {
-			if (result) {
-				if (result.params.refresh_token) {
-					await SecureStore.setItemAsync(
-						authorizerRefreshTokenKey,
-						result.params.refresh_token
-					);
-				}
+            setEmail(jwtDecode(res.id_token).email);
+          } catch (err) {
+            console.error(JSON.stringify(err));
+            await SecureStore.deleteItemAsync(authorizerRefreshTokenKey);
+          }
+        }
+      } catch (error) {
+        setEmail(null);
+        await SecureStore.deleteItemAsync(authorizerRefreshTokenKey);
+      } finally {
+        setLoading(false);
+      }
+    }
+    silentRefresh();
+  }, []);
 
-				if (result.error) {
-					Alert.alert(
-						'Authentication error',
-						result.params.error_description || 'something went wrong'
-					);
-					return;
-				}
+  useEffect(() => {
+    async function setResult() {
+      if (result) {
+        if (result.params.refresh_token) {
+          await SecureStore.setItemAsync(
+            authorizerRefreshTokenKey,
+            result.params.refresh_token
+          );
+        }
 
-				if (result.type === 'success') {
-					// Retrieve the JWT token and decode it
-					const jwtToken = result.params.id_token;
-					const decoded = jwtDecode(jwtToken);
+        if (result.error) {
+          Alert.alert(
+            "Authentication error",
+            result.params.error_description || "something went wrong"
+          );
+          return;
+        }
 
-					const { email } = decoded;
-					setEmail(email);
-				}
-			}
-		}
-		setResult();
-	}, [result]);
+        if (result.type === "success") {
+          // Retrieve the JWT token and decode it
+          const jwtToken = result.params.id_token;
+          const decoded = jwtDecode(jwtToken);
 
-	const handleLogout = async () => {
-		setLoading(true);
-		setEmail(null);
+          const { email } = decoded;
+          setEmail(email);
+        }
+      }
+    }
+    setResult();
+  }, [result]);
 
-		try {
-			const refreshToken = await SecureStore.getItemAsync(
-				authorizerRefreshTokenKey
-			);
-			await authorizerRef.revokeToken({
-				refresh_token: refreshToken,
-			});
-			await SecureStore.deleteItemAsync(authorizerRefreshTokenKey);
-			await WebBrowser.openAuthSessionAsync(
-				`${authorizerURL}/logout?redirect_uri=${redirectUri}`,
-				'redirectUrl'
-			);
-		} catch (err) {
-			console.log(err);
-		} finally {
-			setLoading(false);
-		}
-	};
+  const handleLogout = async () => {
+    setLoading(true);
+    setEmail(null);
 
-	return (
-		<View style={styles.container}>
-			{loading ? (
-				<Text>Loading...</Text>
-			) : (
-				<View>
-					{email ? (
-						<>
-							<Text style={styles.title}>You are logged in, {email}!</Text>
-							<Button title="Log out" onPress={handleLogout} />
-						</>
-					) : (
-						<Button
-							disabled={!request}
-							title="Log in with Authorizer"
-							onPress={() => promptAsync({ useProxy })}
-						/>
-					)}
-				</View>
-			)}
-		</View>
-	);
+    try {
+      const refreshToken = await SecureStore.getItemAsync(
+        authorizerRefreshTokenKey
+      );
+      await authorizerRef.revokeToken({
+        refresh_token: refreshToken,
+      });
+      await SecureStore.deleteItemAsync(authorizerRefreshTokenKey);
+      await WebBrowser.openAuthSessionAsync(
+        `${authorizerURL}/logout?redirect_uri=${redirectUri}`,
+        "redirectUrl"
+      );
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View>
+          {email ? (
+            <>
+              <Text style={styles.title}>You are logged in, {email}!</Text>
+              <Button title="Log out" onPress={handleLogout} />
+            </>
+          ) : (
+            <Button
+              disabled={!request}
+              title="Log in with Authorizer"
+              onPress={() => promptAsync({ useProxy })}
+            />
+          )}
+        </View>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	title: {
-		fontSize: 20,
-		textAlign: 'center',
-		marginTop: 40,
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 40,
+  },
 });
