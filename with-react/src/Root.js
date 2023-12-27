446c6b1512c705a0db5beb21bb94cb7460ed4595
Lakhan Samani <lakhan.m.samani@gmail.com>
2023-12-27 15:44:33 +0530

    Update js version 2.0.0 & react 1.2.0

--- with-react/src/Root.js
+++ with-react/src/Root.js
@@ -3,17 +3,17 @@ import { AuthorizerProvider } from '@authorizerdev/authorizer-react';
 import App from './App';
 
 export default function Root() {
-	return (
-		<BrowserRouter>
-			<AuthorizerProvider
-				config={{
-					authorizerURL: 'https://demo.authorizer.dev',
-					redirectURL: window.location.origin,
-					clientID: '96fed66c-9779-4694-a79a-260fc489ce33',
-				}}
-			>
-				<App />
-			</AuthorizerProvider>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <AuthorizerProvider
+        config={{
+          authorizerURL: `https://demo.authorizer.dev`,
+          redirectURL: window.location.href,
+          clientID: '96fed66c-9779-4694-a79a-260fc489ce33',
+        }}
+      >
+        <App />
+      </AuthorizerProvider>
+    </BrowserRouter>
+  );
 }
